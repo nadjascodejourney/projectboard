9e6846b6a46824f7e2607d77b005dcdfc9cb6b8a
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-12-07 13:31:31 +0100

    aboutpage: small content change

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -8,18 +8,9 @@ export default function AboutPage() {
         name="Nadja Probst"
         profession="Webdeveloper & UX/UI Designer"
         introduction={`
-          Hello! I'm a web developer with more than 1 year experience in creating web applications and 6 years of former experience in museum education. Before that, I graduated in Fine Arts and aesthetic education at the University of Regensburg. 
-          
-          
-
-
-          I really like to work with React and recently I discoverd the advantages of TypeScript. My approach to development is centered around creating clean, maintainable code that scales well and provides an excellent user experience. 
-          I'm curious and therefore, I like to learn new technologies and methodologies. I'm committed to using my skills to create meaningful solutions. 
-          
-          
-
-
-          When I'm not coding, you can find me reading, drawing or playing with my cats. `}
+          Hello! My name is Nadja and I'm a web developer and UX/UI designer. 
+          I love to create websites and applications and challenge myself by learning new tools and technologies to find the best solution for the problem at hand.
+          When I'm not coding, you can find me reading, drawing, or watching cat videos. `}
         profileImageUrl="/icons/Nadja_Probst-black.jpg"
       />
     </div>
