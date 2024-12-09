d8a1d1ddc9e80f45609caf2fefc59bba2426b609
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-12-09 21:58:39 +0100

    page/homepage: added welcome 3d object

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+import Spline from "@splinetool/react-spline";
+
+export default function HomePage() {
+  return (
+    <Spline scene="https://prod.spline.design/5AuydLBp7vqilqqh/scene.splinecode" />
+  );
+}
