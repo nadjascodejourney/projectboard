d3324eb2ad4057e778c950646e51d5c7ded29164
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-12-09 22:23:51 +0100

    page/homepage: styling of 3d object

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,27 @@ import Spline from "@splinetool/react-spline";
 
 export default function HomePage() {
   return (
-    <Spline scene="https://prod.spline.design/5AuydLBp7vqilqqh/scene.splinecode" />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+        position: "relative", // Wichtig, um andere Positionierungen zu erlauben
+      }}
+    >
+      <Spline
+        scene="https://prod.spline.design/5AuydLBp7vqilqqh/scene.splinecode"
+        style={{
+          position: "absolute", // Überlagert den Viewport
+          transform: "translate(-50%, -50%)", // Zentriert relativ zum Mittelpunkt
+          top: "50%", // Vertikal zentriert
+          left: "50%", // Horizontal zentriert
+          width: "80%", // Optionale Größenanpassung
+          height: "80%", // Optionale Größenanpassung
+        }}
+      />
+    </div>
   );
 }
